650fe510802fc15270817b9e9b3a60ec8efe94c7
gabriel-francelino <90209521+gabriel-francelino@users.noreply.github.com>
2022-05-31 22:41:56 -0300

    Update aula01.cpp
    começo da implementação de uma lista que não se sabe se esta correto.

--- aula01.cpp
+++ aula01.cpp
@@ -1,4 +1,5 @@
-# include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <fstream>
 #include <iostream>
 #include <cmath>
@@ -6,112 +7,137 @@
 
 using namespace std;
 
-int nLinhas = 0, i=0, j=0;
+int nLinhas = 0, i = 0, j = 0;
 
-int leGrafo(const char *name){
+//----------------------------------------------------------------------------------------------------------
+typedef struct grafo Grafo;
+
+struct grafo {
+    int numVertices;
+    int** pesos;
+};
+
+Grafo* iniciaGrafo(int numVertices) {
+    Grafo *gr = (Grafo*) malloc(sizeof (struct grafo));
+    if (gr != NULL) {
+        gr->numVertices = numVertices;
+        for (i = 0; i < numVertices; i++) {
+            gr->pesos = (int**) malloc(numVertices * sizeof (int*));
+            for (j = 0; j < numVertices; j++) {
+                gr->pesos[j] = (int*) malloc(numVertices * sizeof (int));
+            }
+        }
+    }
+    return gr;
+}
+
+void liberaGrafo(Grafo* gr){
+    if(gr != NULL){
+        for(i=0; i<gr->numVertices; i++){
+            free(gr->pesos[i]);
+        }
+        free(gr->pesos);
+        free(gr);
+    }
+}
+//-----------------------------------------------------------------------------------
+int leGrafo(const char *name) {
     ifstream myfile(name);
     if (myfile.is_open()) {
         while (!myfile.eof()) {
             //ler o tipo
             int a, b, c, n;
-            
+
             myfile >> a;
             myfile >> b;
             myfile >> c;
-            nLinhas++;  
+            nLinhas++;
         }
         myfile.close();
-    } else{
+    } else {
         cout << "NÃ£o foi possÃ­vel ler o arquivo." << endl;
     }
-        
+
     return 0;
 }
 
-int *x = (int *) malloc(nLinhas * sizeof(int));
-int *y = (int *) malloc(nLinhas * sizeof(int));
+int *x = (int *) malloc(nLinhas * sizeof (int));
+int *y = (int *) malloc(nLinhas * sizeof (int));
 
-int coordenadas(const char *name){
+int coordenadas(const char *name) {
     ifstream myfile(name);
     if (myfile.is_open()) {
         while (!myfile.eof()) {
 
             int a;
-            
+
             myfile >> a;
             myfile >> x[i];
             myfile >> y[i];
-            i++;  
+            i++;
         }
         myfile.close();
-    } else{
+    } else {
         cout << "NÃ£o foi possÃ­vel ler o arquivo." << endl;
     }
-        
+
     return 0;
 }
 
-int peso(int *x, int *y, int i, int j){
-    int p,a,b;
-    a=x[j]-x[i];
-    b=y[j]-y[i];
-    p = sqrt(pow(a,2) + pow(b,2));
+int peso(int *x, int *y, int i, int j) {
+    int p, a, b;
+    a = x[j] - x[i];
+    b = y[j] - y[i];
+    p = sqrt(pow(a, 2) + pow(b, 2));
     return p;
 }
 
-void imprime(){
-    for (int i = 0; i < nLinhas; i++)
-    {
+void imprime() {
+    for (int i = 0; i < nLinhas; i++) {
         cout << "x: " << x[i] << "\ty: " << y[i] << endl;
     }
 }
 
-void imprimeMatriz(int **m){
-    for ( i = 0; i < nLinhas; i++)
-    {
-        for ( j = 0; j < nLinhas; j++)
-        {
-            cout << "["<< m[i][j] << "]\t";
+void imprimeMatriz(int **m) {
+    for (i = 0; i < nLinhas; i++) {
+        for (j = 0; j < nLinhas; j++) {
+            cout << "[" << m[i][j] << "]\t";
         }
         cout << endl;
     }
 }
 
-int **matrizAloc(){
+int **matrizAloc() {
     int **mat;
-    mat = (int **) malloc (nLinhas * sizeof (int*)) ;
-    for (i=0; i < nLinhas; i++){
-        mat[i] = (int *) malloc (nLinhas * sizeof (int)) ;
+    mat = (int **) malloc(nLinhas * sizeof (int*));
+    for (i = 0; i < nLinhas; i++) {
+        mat[i] = (int *) malloc(nLinhas * sizeof (int));
     }
 
-   for (int lin = 0; lin < nLinhas; lin++)
-   {
-       for (int col = 0; col < nLinhas; col++)
-       {
-           if (lin == col)
-           {
-               mat[lin][col] = 0;
-           }else{
-               mat[lin][col] = peso(x, y, lin, col);
-           }
-       }
-       
-   }
+    for (int lin = 0; lin < nLinhas; lin++) {
+        for (int col = 0; col < nLinhas; col++) {
+            if (lin == col) {
+                mat[lin][col] = 0;
+            } else {
+                mat[lin][col] = peso(x, y, lin, col);
+            }
+        }
+
+    }
     return mat;
 }
 
-void freeMatriz(int **m){
-    for(i=0; i<nLinhas; i++){
+void freeMatriz(int **m) {
+    for (i = 0; i < nLinhas; i++) {
         free(m[i]);
     }
     free(m);
 }
 
+int main(int argc, char**argv) {
 
-int main(int argc, char**argv){
-
-    leGrafo("aula3aeds.txt");
-    coordenadas("aula3aeds.txt");
+    leGrafo("teste.txt");
+    coordenadas("teste.txt");
     //imprime();
     int **m = matrizAloc();
     imprimeMatriz(m);
